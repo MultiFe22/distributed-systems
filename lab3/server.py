2b064c81a772af38a029915cfe675d21f6fd0e50
Felipe Vidal <felipevidal@sinaitechnologies.com>
2023-05-29 14:46:13 -0300

    Updating lab3

--- lab3/server.py
+++ lab3/server.py
@@ -1,46 +1,56 @@
 import rpyc
 import os
 
+# Load dictionary from backup
+dictionary = {}
+with open("backup.txt", "r") as backup:
+    for line in backup.readlines():
+        line = line.strip()
+        key, value = line.split(":")
+        values = dictionary.get(key, [])
+        values.append(value)
+        dictionary[key] = values
+print("Dictionary loaded from backup file.")
+
 class MyService(rpyc.Service):
     def on_connect(self, conn):
-        # Load dictionary from backup
-        self.dictionary = {}
-        with open("backup.txt", "r") as backup:
-            for line in backup.readlines():
-                line = line.strip()
-                key, value = line.split(":")
-                values = self.dictionary.get(key, [])
-                values.append(value)
-                self.dictionary[key] = values
-        print("Dictionary loaded from backup file.")
+        print("Client connected.")
     
     def on_disconnect(self, conn):
         # Save dictionary to backup
         with open("backup.txt", "w") as backup:
-            for key, values in self.dictionary.items():
+            for key, values in dictionary.items():
                 for value in values:
                     backup.write(f"{key}:{value}\n")
         print("Dictionary saved to backup file.")
+        print("Client disconnected.")
 
     def exposed_set(self, key, value):
         # Insert new key-value pair
-        values = self.dictionary.get(key, [])
+        if key in dictionary:
+            already_exists = True
+        else:
+            already_exists = False
+        values = dictionary.get(key, [])
         values.append(value)
-        self.dictionary[key] = values
-        return f"Key '{key}' inserted successfully."
+        dictionary[key] = values
+        if already_exists:
+            return f"Key '{key}' updated successfully."
+        else:
+            return f"Key '{key}' inserted successfully."
 
     def exposed_get(self, key):
         # Query the value associated with the key
-        if key in self.dictionary:
-            values = self.dictionary.get(key, [])
+        if key in dictionary:
+            values = dictionary.get(key, [])
             return "\n".join(sorted(values))
         else:
             return f"Key '{key}' not found."
 
     def exposed_remove(self, key):
         # Remove the key-value pair
-        if key in self.dictionary:
-            self.dictionary.pop(key, None)
+        if key in dictionary:
+            dictionary.pop(key, None)
             return f"Key '{key}' removed successfully."
         else:
             return f"Key '{key}' not found."
